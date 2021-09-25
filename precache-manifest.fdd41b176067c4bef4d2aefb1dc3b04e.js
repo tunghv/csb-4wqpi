baea8fc3f8da157d27435fcc8920c30cc3fd0677
Code Sandbox <hello@codesandbox.io>
2021-09-25 05:21:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.fdd41b176067c4bef4d2aefb1dc3b04e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977359c5162352194b81215248dd5cda",
+    "url": "https://tunghv.github.io/csb-4wqpi/index.html"
+  },
+  {
+    "revision": "a5e6c7f015f3bea04b8f",
+    "url": "https://tunghv.github.io/csb-4wqpi/static/js/2.763b5d5a.chunk.js"
+  },
+  {
+    "revision": "ffce31e1feadddb94b52",
+    "url": "https://tunghv.github.io/csb-4wqpi/static/js/main.65ff8b9d.chunk.js"
+  },
+  {
+    "revision": "715d6822fbbc1d8b4ca8",
+    "url": "https://tunghv.github.io/csb-4wqpi/static/js/runtime~main.92a8f886.js"
+  }
+]);
